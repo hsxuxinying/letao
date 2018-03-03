f06ed3bb7055a3059b8f819e46f9443d8dc49edc
unknown <1668835859@qq.com>
2018-03-03 21:47:37 +0800

    进度条完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,17 @@
 /**
  * Created by Me on 2018/3/2.
  */
+$(function(){
+    //禁用缓冲圈
+    NProgress.configure(
+        { showSpinner: false }
+    );
+    //开启进度条
+    $(document).ajaxStart(function () {
+        NProgress.start();
+    });
+    $(document).ajaxStop(function(){
+        NProgress.done();
+    })
+
+});
